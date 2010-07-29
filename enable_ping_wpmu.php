b68436d7e3e205704d09a15e0ad842840f4485f1
sean212 <sean212@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-29 20:20:06 +0000

    Initial release
    git-svn-id: https://plugins.svn.wordpress.org/enable-site-ping-wpmu/trunk@268978 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ enable_ping_wpmu.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: Enable Site Ping WPMU
+Plugin URI: http://sean-fisher.com
+Description: Enable site ping for a WPMU site.
+Version: 1.0
+Author: Sean Fisher
+Author URI: http://sean-fisher.com/
+*/
+
+/**
+ *	Enabling the site ping input on Options -> Writing.
+**/
+function enable_ping() {
+	remove_filter('enable_update_services_configuration', '__return_false');
+	add_filter( 'enable_update_services_configuration', '_do_return_true' );
+}
+
+/**
+ *	Return true
+**/
+function _do_return_true() {
+	return true;
+}
+
+/**
+ *	Allow the user to update the ping list.
+**/
+function enable_ping_whitelist($list = NULL) {
+	if ( is_null( $list )) 
+		$list = array( );
+	
+	//	Removing it if it already existed.
+	foreach($list['writing'] as $key => $writing) {
+		if ($writing == 'ping_sites') 
+			unset($list['writing'][$key]);
+	}
+	
+	$list['writing'][] = 'ping_sites';
+	return $list;	
+}
+
+/**
+ *	Making sure the ping list exists.
+**/
+if ( !has_action('do_pings', 'do_all_pings') )
+	add_action( 'do_pings',' do_all_pings', 10, 1 );
+
+//	Filters
+add_filter('whitelist_options', 'enable_ping_whitelist');
+add_action('init', 'enable_ping');
+
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Enable Site Ping WPMU ===
+Contributors: talkingwithsean
+Tags: wpmu, ping, google, posts
+Requires at least: 2.5
+Tested up to: 3.0
+Stable tag: 1.0
+
+== Description ==
+
+We are going to enable the WordPress ping service for each site in a WordPress MU Network. This allows each site to have a different ping list. By default, you have no ping list for WordPress MU sites.
+
+= New =
+
+* Nothing
+
+= Features =
+
+* Enables the custom ping for WordPress sites in WP MU.
+* Individual site settings for WPMU.
+
+
+== Installation ==
+
+Follow the steps below to install the plugin.
+
+1. Upload the Site Ping Plugin directory to the /wp-content/plugins/ directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to <a href="options-writing.php">Settings -> Writing</a> to update the ping list.
+
+== Help ==
+
+For help, checkout http://sean-fisher.com/plugins/
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release
